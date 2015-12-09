c9dca973a2a2838e884e00f99f6a891908d65711
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-09 13:20:21 +0100

    badge over progressbar

--- js/profile_app.js
+++ js/profile_app.js
@@ -1,2 +1,8 @@
 $(function (){
+	$(document).on("mouseenter", ".progress",function(){
+		$(this).find("span").fadeIn();
+	});
+	$(document).on("mouseleave", ".progress", function(){
+		$(this).find("span").fadeOut();
+	});
 });
