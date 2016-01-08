a590371939ffb7517f4663662fb5ed42bf5d2001
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-01-08 13:23:48 +0100

    summary changes

--- js/profile_app.js
+++ js/profile_app.js
@@ -1,14 +1,11 @@
 $(function (){
-	$(document).on("mouseenter", ".progress",function(){
-		$(this).find("span").fadeIn();
-	});
-	$(document).on("mouseleave", ".progress", function(){
-		$(this).find("span").fadeOut();
-	});
 	$(document).on("click", ".profile-menu a", function(){
 		if (!$(this).hasClass("active")) {
 			$(".profile-menu a").removeClass("active");
 			$(this).addClass("active");
 		}
+	});
+	$(document).on("click", "#likeButton", function(){
+		$(this).find("img").attr("src", "images/liked.png")
 	});
 });
